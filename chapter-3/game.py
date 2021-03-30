03f935666bb2850dc1937d5e6b12c0a2446998c2
Nathan Pham <nathanpham.me@gmail.com>
2021-03-29 23:54:03 -0700

    y margin

--- chapter-3/game.py
+++ chapter-3/game.py
@@ -2,6 +2,8 @@ import pygame, random
 from config import *
 from colors import *
 
+print(X_MARGIN, Y_MARGIN)
+
 def scale_coordinates(x, y):
     scaled_x = x * (BOX_SIZE + GAP_SIZE) + X_MARGIN
     scaled_y = y * (BOX_SIZE + GAP_SIZE) + Y_MARGIN
@@ -25,6 +27,24 @@ class Card:
 
     def render(self, ctx):
         (shape, color) = self.icon
+        (left, top) = scale_coordinates(self.x, self.y)
+
+        QUARTER = int(BOX_SIZE / 4)
+        HALF = int(BOX_SIZE / 2)
+
+        if shape == DONUT:
+            pygame.draw.circle(ctx, color, (left + HALF, top + HALF), HALF - 5)
+            pygame.draw.circle(ctx, THEME_BG, (left + HALF, top + HALF), QUARTER - 5)
+        elif shape == SQUARE:
+            pygame.draw.rect(ctx, color, (left + QUARTER, top + QUARTER, BOX_SIZE - HALF, BOX_SIZE - HALF))
+        elif shape == DIAMOND:
+            pygame.draw.polygon(ctx, color, ((left + HALF, top), (left + BOX_SIZE - 1, top + HALF), (left + HALF, top + BOX_SIZE - 1), (left, top + HALF)))
+        elif shape == LINES:
+            for i in range(0, BOX_SIZE, 4):
+                pygame.draw.line(ctx, color, (left, top + i), (left + i, top))
+                pygame.draw.line(ctx, color, (left + i, top + BOX_SIZE - 1), (left + BOX_SIZE - 1, top + i))
+        elif shape == OVAL:
+            pygame.draw.ellipse(ctx, color, (left, top + QUARTER, BOX_SIZE, HALF))
 
 
 def generate_board(width, height):
@@ -40,12 +60,12 @@ def generate_board(width, height):
 
     game_board = []
     for x in range(BOARD_WIDTH):
-        column = []
+        # column = []
         for y in range(BOARD_HEIGHT):
-            column.append(Card(possible_icons[0], x, y))
+            game_board.append(Card(possible_icons[0], x, y))
             del possible_icons[0]
 
-        game_board.append(column)
+        # game_board.append(column)
 
     return game_board
 
--- chapter-3/main.py
+++ chapter-3/main.py
@@ -2,6 +2,7 @@ import pygame, sys
 from pygame.locals import *
 from config import *
 from colors import *
+import game
 
 pygame.init()
 
@@ -12,6 +13,7 @@ clock = pygame.time.Clock()
 mouse_x = 0
 mouse_y = 0
 mouse_clicked = False
+game_board = game.generate_board(WINDOW_SIZE[0], WINDOW_SIZE[1])
 
 while True:
     display.fill(THEME_BG)
@@ -25,6 +27,9 @@ while True:
         elif event.type == MOUSEBUTTONUP:
             (mouse_x, mouse_y) = event.pos
             mouse_clicked = True
+
+    for card in game_board:
+        card.render(display)
 
     pygame.draw.circle(display, RED, (mouse_x, mouse_y), 10, 2)
 
